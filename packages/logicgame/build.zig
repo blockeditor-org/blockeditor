006a43f9164eefced57afa39c053ffa4e9f0d4a7
pfg <pfg@pfg.pw>
2025-10-24 22:44:53 -0700

    had it backwards

--- packages/logicgame/build.zig
+++ packages/logicgame/build.zig
@@ -22,7 +22,7 @@ pub fn build(b: *std.Build) void {
     });
     const dat_test = b.addTest(.{
         .root_module = dat,
-        .use_llvm = false, // https://github.com/ziglang/zig/issues/25247
+        .use_llvm = true, // https://github.com/ziglang/zig/issues/25247
     });
 
     b.installArtifact(dat_test);
