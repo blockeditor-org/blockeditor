dd9b31902eec183d8280832628bef2a78243f91d
pfg <pfg@pfg.pw>
2025-10-25 23:30:10 -0700

    rm s & s2 modes

--- packages/cvl2/src/cvl2.ts
+++ packages/cvl2/src/cvl2.ts
@@ -466,7 +466,7 @@ export function tokenize(source: Source): TokenizationResult {
 
 interface RenderConfig {
     indent: string;
-    style?: "s" | "s2" | "adisp";
+    style?: "adisp";
 }
 
 function renderEntityList(config: RenderConfig, entities: SyntaxNode[], level: number, isTopLevel: boolean): string {
@@ -547,50 +547,8 @@ function renderEntityJ(entity: SyntaxNode): unknown {
         TODO: true,
     };
 }
-function renderS(config: RenderConfig, tag: string, s: (SyntaxNode | string)[], level: number, s0?: string, inh?: boolean): string {
-    const sub = (itm: string | SyntaxNode, level: number) => (typeof itm === "string" ? itm : renderEntity(config, itm, level, false));
-    if(s.length === 0) return `${tag}(${s0 ?? ""})`;
-    if(s.length === 1 && !s0 && !inh) return `${tag}(${s0 ? s0 + " " : ""}${sub(s[0]!, level)})`;
-    return tag + "(" + (s0 ?? "") + s.map(itm => "\n" + config.indent.repeat(level + 1) + sub(itm, level + 1)).join("") + "\n" + config.indent.repeat(level) + ")";
-}
 function renderEntity(config: RenderConfig, entity: SyntaxNode, level: number, isTopLevel: boolean): string {
-    if(config.style === "s2") {
-        if(entity.kind === "block") {
-            return renderS(config, "block", entity.items, level, JSON.stringify(entity.start + entity.end));
-        }else if(entity.kind === "binary") {
-            return "binary " + entity.items.map(item => renderEntity(config, item, level, false)).join(" ");
-        }else if(entity.kind === "ident") {
-            return "@" + entity.str;
-        }else if(entity.kind === "op") {
-            return "<" + JSON.stringify(entity.op) + ">";
-        }else if(entity.kind === "opSeg") {
-            return renderS(config, "", entity.items, level, undefined, true);
-        }else if(entity.kind === "strSeg") {
-            return renderS(config, "strSeg", [], level, JSON.stringify(entity.str));
-        }else if(entity.kind === "ws") {
-            return entity.nl ? `"\\n"` : `" "`;
-        }else{
-            return renderS(config, (entity as SyntaxNode).kind, [], level);
-        }
-    }else if (config.style === "s") {
-        if (entity.kind === "block") {
-            return `${JSON.stringify(entity.start + entity.end)} ` + renderEntityList(config, entity.items, level, false) + ")";
-        } else if (entity.kind === "binary") {
-            return "(" + renderEntityList(config, entity.items, level, isTopLevel) + ")";
-        } else if (entity.kind === "ws") {
-            throw new Error("Unreachable: Whitespace should be handled by renderEntityList.");
-        } else if (entity.kind === "ident") {
-            return "$" + entity.str;
-        } else if (entity.kind === "op") {
-            return JSON.stringify(entity.op);
-        } else if (entity.kind === "opSeg") {
-            return "(" + renderEntityList(config, entity.items, level, isTopLevel) + ")";
-        } else if (entity.kind === "strSeg") {
-            return JSON.stringify(entity.str);
-        } else {
-            return `(TODO $${(entity as {kind: string}).kind})`;
-        }
-    }else if(config.style === "adisp") {
+    if(config.style === "adisp") {
         const ch: SyntaxNode[] | undefined = entity.kind === "block" || entity.kind === "binary" || entity.kind === "opSeg"  ? entity.items : undefined;
         let desc: string;
         if (entity.kind === "block") {
@@ -686,16 +644,12 @@ function prettyPrintErrors(source: Source, errors: TokenizationError[]): string 
 
 export function renderTokenizedOutput(tokenizationResult: TokenizationResult, source: Source): string {
     const formattedCode = renderEntityList({ indent: "  " }, tokenizationResult.result, 0, true);
-    const sExpression = renderEntityList({ indent: "  ", style: "s" }, tokenizationResult.result, 0, true);
-    const s2Expression = renderEntityList({ indent: "  ", style: "s2" }, tokenizationResult.result, 0, true);
     const adisp = renderEntityList({indent: "│ ", style: "adisp"}, tokenizationResult.result, 0, true);
     const jsonCode = JSON.stringify(tokenizationResult.result.map(renderEntityJ), null, 1);
     const prettyErrors = prettyPrintErrors(source, tokenizationResult.errors);
     
     return (
         `// json:\n${jsonCode}\n\n` +
-        `// s2-expr:\n${s2Expression}\n\n` +
-        `// s-expr:\n${sExpression}\n\n` +
         `// adisp:\n${adisp}\n\n` +
         `// formatted\n${formattedCode}\n\n` +
         `// errors:\n${prettyErrors}`
